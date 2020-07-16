73570be9f44904c5f4815f38ee821c3252a95945
Debora Baddipudi <mailtodeborah1@gmail.com>
2020-07-16 03:04:53 +0000

    added Procfile

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from flask import Flask , render_template, request, flash
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 
-@app.route("/index.html")
+@app.route('/')
 def index():
     return render_template("index.html")
 
