fe026501f7bb43b590ec27e7eb22bd12e2be92d0
Debora Baddipudi <mailtodeborah1@gmail.com>
2020-07-16 02:53:12 +0000

    corrected route for index.html

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from flask import Flask , render_template, request, flash
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 
-@app.route("/")
+@app.route("/index.html")
 def index():
     return render_template("index.html")
 
