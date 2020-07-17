498c1e3e12d74d38768df990e6884bfde140b903
Debora Baddipudi <mailtodeborah1@gmail.com>
2020-07-17 17:23:21 +0000

    added chat functionalitty

--- run.py
+++ run.py
@@ -1,13 +1,47 @@
 import os
-import json
-from flask import Flask , render_template, request, flash
+from datetime import datetime
+from flask import Flask, json, redirect, render_template, request, session, url_for
 
+
+app = Flask(__name__)
+app.secret_key = "randomstring123"
+messages = []
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 
 @app.route('/')
 def index():
+    return render_template("index.html",page_title="Ananya Caterers")
+
+def add_message(username, message):
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append({"timestamp": now, "from": username, "message": message})
+
+
+@app.route("/", methods=["GET", "POST"])
+def chat():
+    """Main page with instructions"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
+
     return render_template("index.html")
+
+
+@app.route("/chat/<username>", methods=["GET", "POST"])
+def user(username):
+    """Add and display chat messages"""
+    if request.method == "POST":    
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("chat.html", username=username,chat_messages=messages,page_title="Chat with Us")
+
 
 
 @app.route("/ourspecialities")
@@ -27,7 +61,7 @@ def contact():
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html", age_title = "Come work with us")
+    return render_template("careers.html", page_title = "Come work with us")
 
 
 if __name__ == '__main__':
